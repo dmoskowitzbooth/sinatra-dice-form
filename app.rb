6e39b5471d8b6b92e2cf7863142d6b276346da51
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-17 00:58:55 +0000

    2

--- app.rb
+++ app.rb
@@ -9,5 +9,16 @@ get("/") do
 end
 
 get("/process_roll") do
-  erb(:results)
+  @num_dice=params.fetch("dice").to_i
+
+  @num_sides=params.fetch("sides").to_i
+
+  @rolls=[]
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
+erb(:results)
+
 end
